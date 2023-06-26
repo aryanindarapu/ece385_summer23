ad0703ec0da8f930c2e686e5eaae28de9e2b7922
Aryan Indarapu <aryan1pro@gmail.com>
2023-06-25 22:25:49 -0500

    added top level lab3 file

---
+++ lab3/multiplier.sv
@@ -0,0 +1,9 @@
+module multiplier (
+    input logic [7:0] SW,
+    input logic Clk, Reset_Load_Clear, Run,
+    output logic [6:0] HEX0, HEX1, HEX2, HEX3,
+    output logic [7:0] Aval, Bval,
+    output logic Xval 
+)
+
+endmodule

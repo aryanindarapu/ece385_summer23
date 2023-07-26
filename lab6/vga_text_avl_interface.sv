b9ae1f6f501e7885896377edbf5b1a664395973a
Aryan Indarapu <aryan1pro@gmail.com>
2023-07-26 00:28:24 -0500

    mini changes in avl_interface

--- lab6/vga_text_avl_interface.sv
+++ lab6/vga_text_avl_interface.sv
@@ -116,9 +116,9 @@ always_ff @(posedge CLK) begin
 	end
 
 	// 80x30 = 2400 glyphs | row*80 + col will give our glyph/char address
-	char_index <= (row*80) + col;
+	char_index <= (row*80) + col; // which row-major form byte index are we on
 	// (char_addr / 4) = word_addr/register
-	word_addr <= char_index[10:2];
+	word_addr <= char_index[10:2]; // range from 0 to 600
 	// now pixel_data is the correct 8 bit data
 	bof <= pixel_data[glyph_col];
 	// byte in the word -> char_addr % 4 
@@ -138,16 +138,16 @@ always_comb begin
 	// now we know the word address and byte we are drawing in, so we can access what character we are drawing
 	case (curr_byte) 
 		11'b00000000001: begin // 1st byte of the word
+			glyph_addr[10:4] = LOCAL_REG[word_addr][14:8]; // basically just the code (i.e. which glyph)
+			glyph_addr[3:0] = glyph_row[3:0]; // how far down in each glyph we are
+			inv_en = LOCAL_REG[word_addr][15];
+		end
+		11'b00000000010: begin // 2nd byte of the word
 			glyph_addr[10:4] = LOCAL_REG[word_addr][22:16];
 			glyph_addr[3:0] = glyph_row[3:0];
 			inv_en = LOCAL_REG[word_addr][23];
 		end
 		11'b00000000011: begin // 3rd byte of the word
-			glyph_addr[10:4] = LOCAL_REG[word_addr][14:8];
-			glyph_addr[3:0] = glyph_row[3:0];
-			inv_en = LOCAL_REG[word_addr][15];
-		end
-		11'b00000000010: begin // 2nd byte of the word
 			glyph_addr[10:4] = LOCAL_REG[word_addr][30:24];
 			glyph_addr[3:0] = glyph_row[3:0];
 			inv_en = LOCAL_REG[word_addr][31];
